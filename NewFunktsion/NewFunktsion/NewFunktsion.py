ee7f3e8dbc5f4808c19c43be1880400c0ce51708
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-02-08 01:05:41 +0300

    Update NewFunktsion.py

--- NewFunktsion/NewFunktsion/NewFunktsion.py
+++ NewFunktsion/NewFunktsion/NewFunktsion.py
@@ -4,32 +4,26 @@ valeIsikukood=[]
 haigla=[]
 isikukoodi=[]
 arvud=[]
-while True: 
+
+while True:
     isikukood=input("Sisse ikood: ")
-    jaak=kontrollnumber(isikukood)
     if pikkus(isikukood)==False:
         print("4to-to ne tak")
+        valeIsikukood.append(isikukood)
     else:
+        jaa=kontrollnr(isikukood)
+        if jaa==False:
+            print("4to-to ne tak")
+            valeIsikukood.append(isikukood)
         print("11 sümbolid")
         s=sugu(isikukood)
-        if s=="viga" or jaak=="Viga":
-            print("Esimene täht ei ole õige") 
-            valeIsikukood.append(isikukood)
-        else:
-            õigeIsikukood.append(isikukood)
-            print(s)
-            aasta=sünnipäev(isikukood)
-            mesjac=kuu(isikukood)
-            paev=päev(isikukood)
-            sünnikoht=sunnikoht(isikukood)
-            print(jaak)
-            haigla.append(sünnikoht)
-            print(f"{aasta}.{mesjac}.{paev}, {sünnikoht}, {jaak}")
-    arvudSort(valeIsikukood)
-    arvudSort(õigeIsikukood)
-    haigla.sort()
-    print(f"vale isikukood, {valeIsikukood}")
-    print(f"õige isikukood, {õigeIsikukood}")
-    print(f"haigla, {haigla}")
-    print(arvud)
-    print(isikukoodi)
+        sunni=sünnipäev(isikukood)
+        kui=kuu(isikukood)
+        paev=päev(isikukood)
+        koht=sunnikoht(isikukood)
+        print(f"{s}, {sunni}.{kui}.{paev} ,{koht}, {jaa}")
+        õigeIsikukood.append(isikukood)
+        haigla.append(koht)
+    print("Vale isikukood", valeIsikukood)
+    print("Õige isikukood", õigeIsikukood)
+    print("Haigla", haigla)
